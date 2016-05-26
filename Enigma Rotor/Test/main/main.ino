c9684a3975f5a5dcfa28ef9d36c6c190bc4cd1e1
Gait <www.gertjan007@live.nl>
2016-05-26 13:32:36 +0200

    Added comments
    I added comments to the code

--- Enigma Rotor/Test/main/main.ino
+++ Enigma Rotor/Test/main/main.ino
@@ -1,98 +1,100 @@
-
-/*
-* I2CPortExpanderX
-*/
-
-
 #include <avr/io.h>
-#include "USART.h"
+#include <Wire.h>
 #include <util/delay.h>
 #include <Arduino.h>					//	nodig voor de init()
 #include <string.h>
 #include "Rotor.h"
 
-MY_USART myUsart;
 char command[30];
+char result;
+uint8_t inD2, inD3 = 0;
+uint8_t portAddress;
 Rotor rotor = Rotor();
+
+// Makes the INT1_vect use the ISR of INT0_vect
+ISR(INT1_vect, ISR_ALIASOF(INT0_vect));
+
+// The interrupt changes the address
+ISR(INT0_vect) {
+	inD2 = ((PIND & B00000100) >> 2);
+	inD3 = ((PIND & B00001000) >> 2);
+	portAddress = (B00001100 | inD2 | inD3);
+	Wire.end();
+	Wire.begin(portAddress);
+}
 
 int main(void)
 {
 	init();								//	van arduino.h
-	_delay_ms(100);
 
-	myUsart.initUSART();				//	Serial.begin(9600);
-	_delay_ms(100);
+	// Initializes the interrupts
+	sei();
+	EICRA |= (1 << ISC10) | (1 << ISC00);
+	EIMSK |= (1 << INT1) | (1 << INT0);
 
-	DDRB |= (1 << PORTB5);
-	PORTB |= (1 << PORTB5);
-	//Test code
-	myUsart.println("hallo/\n");
+	// Gets the address of the slave
+	inD2 = ((PIND & B00000100) >> 2);
+	inD3 = ((PIND & B00001000) >> 2);
+
+	// Sets the address of the slave
+	portAddress = (B00001100 | inD2 | inD3);
+
+	Wire.begin(portAddress);
+	Wire.onReceive(receiveCommand);
+	Wire.onRequest(sendCommand);
+
 	while (1)
 	{
-		receiveCommand();
 		// compares the input command to a string
-		if (strcmp(command, "test") == 0) {
-			PORTB ^= (1 << PORTB5);
-			myUsart.println("toggled/");
-		} else if(strcmp(command, "rotateForward") == 0){
+		if(strcmp(command, "rotateForward") == 0){
 			rotor.rotateForward();
 		} else if (strcmp(command, "getRotorLetter") == 0) {
-			myUsart.transmitByte(rotor.getRotorLetter());
-			myUsart.transmitByte('\n');
+			result = rotor.getRotorLetter();
 		} else if (strcmp(command, "rotateBackwards") == 0) {
 			rotor.rotateBackwards();
 		} else if (strncmp(command, "getShiftedLetter", 16) == 0) {
-			myUsart.transmitByte(rotor.getShiftedLetter(command[strlen(command) - 1]));
-			myUsart.transmitByte('\n');
+			result = rotor.getShiftedLetter(command[strlen(command) - 1]);
 		} else if (strncmp(command, "getLetter", 9) == 0) {
-			myUsart.transmitByte(rotor.getLetter(command[strlen(command) - 1]));
-			myUsart.transmitByte('\n');
+			result = rotor.getLetter(command[strlen(command) - 1]);
 		} else if (strncmp(command, "getReverseLetter", 16) == 0) {
-			myUsart.transmitByte(rotor.getReverseLetter(command[strlen(command) - 1]));
-			myUsart.transmitByte('\n');
+			result = rotor.getReverseLetter(command[strlen(command) - 1]);
 		} else if (strncmp(command, "changeRingCfg", 13) == 0) {
 			rotor.changeRingConfiguration((uint8_t)command[strlen(command) - 1] - 48);
 		} else if (strncmp(command, "getReverseShiftedLetter", 23) == 0) {
-			myUsart.transmitByte(rotor.getShiftedReverseLetter(command[strlen(command) - 1]));
-			myUsart.transmitByte('\n');
+			result = rotor.getShiftedReverseLetter(command[strlen(command) - 1]);
 		} else if (strncmp(command, "getTriggered", 12) == 0) {
-			if(rotor.getTriggered()) {
-				myUsart.println("De rotor is in de trigger positie");
-			}
-			else {
-				myUsart.println("De rotor is niet in de trigger positie");
-			}
-			myUsart.transmitByte('\n');
+			result = rotor.getTriggered();
 		}
-		else if (strncmp(command, "a", 1) == 0) {
-			myUsart.println("Ontvangen");
-			PORTB ^= (1 << PORTB5);
-		}
-		_delay_ms(100);
 	}
 }
 
 // Receives a command
-char receiveCommand() {
-	char key = myUsart.receiveByte();
+void receiveCommand(int amount) {
 	// Empties the command string
 	emptyCommand();
-	
-	// Loops until the command has ended
-	while (key > 0) {
-		sprintf(command, "%s%c", command, key);
-		key = myUsart.receiveByte();
+	char key;
+	if (Wire.available()) {
+		key = Wire.read();
+	}
 
-		// Used to break the loop on a character
-		if (key == '/') {
-			key = 0;
-		}
+	// Loops until the end character is received
+	while (key > 0) {
+			if (key == '/') {
+				key = 0;
+			}
+			else {
+				sprintf(command, "%s%c", command, key);
+			}
+			key = Wire.read();
 	}
 }
 
 // Sends a command
-void sendCommand(char string[]) {
-	myUsart.println(string);
+void sendCommand() {
+		Wire.write(result);
+		// clears the result and the command
+		result = 0;
+		emptyCommand();
 }
 
 // Empties the command string
@@ -101,9 +103,4 @@ void emptyCommand() {
 	for (int i = 0; command[i] != '\0'; i++) {
 		command[i] = '\0';
 	}
-}
-
-// Prints the command
-void printCommand() {
-	myUsart.println(command);
 }
