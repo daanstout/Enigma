7ad5716370c14db81bb17fecf28c18a282f8ccd9
Gait <www.gertjan007@live.nl>
2016-05-27 09:30:50 +0200

    Added files for I2C
    I added some files that are neccessary for I2C connection

---
+++ I2C files/UART/hello_ucosii.c
@@ -0,0 +1,241 @@
+/*************************************************************************
+* Copyright (c) 2004 Altera Corporation, San Jose, California, USA.      *
+* All rights reserved. All use of this software and documentation is     *
+* subject to the License Agreement located at the end of this file below.*
+**************************************************************************
+* Description:                                                           *
+* The following is a simple hello world program running MicroC/OS-II.The * 
+* purpose of the design is to be a very simple application that just     *
+* demonstrates MicroC/OS-II running on NIOS II.The design doesn't account*
+* for issues such as checking system call return codes. etc.             *
+*                                                                        *
+* Requirements:                                                          *
+*   -Supported Example Hardware Platforms                                *
+*     Standard                                                           *
+*     Full Featured                                                      *
+*     Low Cost                                                           *
+*   -Supported Development Boards                                        *
+*     Nios II Development Board, Stratix II Edition                      *
+*     Nios Development Board, Stratix Professional Edition               *
+*     Nios Development Board, Stratix Edition                            *
+*     Nios Development Board, Cyclone Edition                            *
+*   -System Library Settings                                             *
+*     RTOS Type - MicroC/OS-II                                           *
+*     Periodic System Timer                                              *
+*   -Know Issues                                                         *
+*     If this design is run on the ISS, terminal output will take several*
+*     minutes per iteration.                                             *
+**************************************************************************/
+
+
+#include <stdio.h>
+#include "includes.h"
+#include "i2c_opencores.h"
+
+/* Definition of Task Stacks */
+#define   TASK_STACKSIZE       2048
+OS_STK    task1_stk[TASK_STACKSIZE];
+OS_STK    task2_stk[TASK_STACKSIZE];
+
+/* Definition of Task Priorities */
+
+#define TASK1_PRIORITY      1
+#define TASK2_PRIORITY      2
+
+#define UART_3 0x08200000
+#define UART_2 0x08200008
+#define UART_1 0x08200010
+#define UART_0 0x08200018
+
+char command[40] = "";
+
+char getChar(volatile int *UART_ID)
+{
+int data;
+data = *(UART_ID); // read the RS232_UART data register
+	if (data & 0x00008000) { // check RVALID to see if there is new data
+		char character = data & 0xFF;
+		if(((character >= 'a') && (character <= 'z')) || (character == '/')) {
+			return character;
+		}
+	} else {
+		return '\0';
+	}
+}
+
+void putChar(volatile int *UART_ID, char c)
+{
+int control;
+control = *(UART_ID + 4); // read the RS232_UART control register
+if (control & 0x00FF0000) { // if space, write character, else ignore
+	*(UART_ID) = c;
+}
+}
+
+void sendCommand(volatile int *UART_ID, char string[]) {
+	int i;
+	for(i = 0; i < strlen(string); i++) {
+		putChar(UART_ID, string[i]);
+	}
+}
+
+void getCommand(volatile int *UART_ID) {
+	memset(command, 0, strlen(command));
+	command[0] = '\0';
+	INT8U finished = 0;
+	INT8U characterCount = 0;
+	char currentChar = '\0';
+
+	while(!finished) {
+		currentChar = getChar(UART_ID);
+		if(currentChar != '\0') {
+			if((currentChar >= 'a') || (currentChar = '/')) {
+				if(currentChar == '/') {
+					if(characterCount > 0) {
+						finished = 1;
+					}
+				} else {
+					command[characterCount] = currentChar;
+					characterCount++;
+				}
+			}
+		}
+		currentChar = '\0';
+	}
+	command[characterCount] = '\0';
+}
+
+/* Prints "Hello World" and sleeps for three seconds */
+void task1(void* pdata)
+{
+	        INT8U characterCount = 0;
+
+  while (1)
+  { 
+	  volatile int uartComponent = UART_2;
+    //printf("Hello from task1\n");
+//	  OSTimeDlyHMSM(0, 0, 0, 500);
+//		  sendCommand(uartComponent, "test/");
+//		  printf("Send\n");
+
+	  //OSTimeDlyHMSM(0, 0, 0, 20);
+
+//        	getCommand(uartComponent);
+//
+//        		printf("Commando: %s\n", command);
+//        		if(strcmp(command, "hallo") == 0) {
+//        	        	sendCommand(uartComponent, "test/");
+//
+//        	        	memset(command, 0, strlen(command));
+//        	        	command[0] = '\0';
+//        	        	characterCount = 0;
+//        		}
+
+	  int * I2CA_BASE;
+	  I2CA_BASE = 0x08200000;
+	  * I2CA_BASE = 63;
+
+	  int slave_Address = 12;
+
+	  I2C_init(I2CA_BASE, ALT_CPU_FREQ, 100000);
+	  I2C_start(I2CA_BASE, slave_Address, 0);
+	  I2C_write(I2CA_BASE,3,0);  // write to register 3 command
+
+	  I2C_write(I2CA_BASE,8,0);  // write to starting address of 8
+	  int i;
+	  for (i=0;i<7;i++)   // write the next 8 bytes
+	  {
+	   I2C_write(I2CA_BASE,i+8,0);  //
+	  }
+	   I2C_write(I2CA_BASE,i+8,1);  // write last one with last flag
+	  while ( I2C_start(I2CA_BASE,0x50,0)); // make sure the write is done be fore continuing.
+
+
+//	  int * i2c_txrx;
+//	  i2c_txrx = (i2c_base + 3);
+//	  char test[20] = "getLetter a/";
+//	  int i;
+//
+//	  int * i2c_status;
+//	  i2c_status = (i2c_base + 4);
+//
+//	  int Transmitting = 0;
+//	  Transmitting = *i2c_base & 0x02;
+//
+//	  for(i = 0; i < strlen(test); i++) {
+//		  while(Transmitting) {
+//			  OSTimeDlyHMSM(0, 0, 0, 10);
+//		  }
+//		  * i2c_txrx = test[i];
+//	  }
+
+  }
+}
+/* Prints "Hello World" and sleeps for three seconds */
+void task2(void* pdata)
+{
+  while (1)
+  { 
+    //printf("Hello from task2\n");
+    OSTimeDlyHMSM(0, 0, 3, 0);
+  }
+}
+/* The main function creates two task and starts multi-tasking */
+int main(void)
+{
+  
+  OSTaskCreateExt(task1,
+                  NULL,
+                  (void *)&task1_stk[TASK_STACKSIZE-1],
+                  TASK1_PRIORITY,
+                  TASK1_PRIORITY,
+                  task1_stk,
+                  TASK_STACKSIZE,
+                  NULL,
+                  0);
+              
+               
+  OSTaskCreateExt(task2,
+                  NULL,
+                  (void *)&task2_stk[TASK_STACKSIZE-1],
+                  TASK2_PRIORITY,
+                  TASK2_PRIORITY,
+                  task2_stk,
+                  TASK_STACKSIZE,
+                  NULL,
+                  0);
+  OSStart();
+  return 0;
+}
+
+
+/******************************************************************************
+*                                                                             *
+* License Agreement                                                           *
+*                                                                             *
+* Copyright (c) 2004 Altera Corporation, San Jose, California, USA.           *
+* All rights reserved.                                                        *
+*                                                                             *
+* Permission is hereby granted, free of charge, to any person obtaining a     *
+* copy of this software and associated documentation files (the "Software"),  *
+* to deal in the Software without restriction, including without limitation   *
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,    *
+* and/or sell copies of the Software, and to permit persons to whom the       *
+* Software is furnished to do so, subject to the following conditions:        *
+*                                                                             *
+* The above copyright notice and this permission notice shall be included in  *
+* all copies or substantial portions of the Software.                         *
+*                                                                             *
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  *
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,    *
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE *
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER      *
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING     *
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER         *
+* DEALINGS IN THE SOFTWARE.                                                   *
+*                                                                             *
+* This agreement shall be governed in all respects by the laws of the State   *
+* of California and by the laws of the United States of America.              *
+* Altera does not recommend, suggest or require that this reference design    *
+* file be used in conjunction or combination with any other product.          *
+******************************************************************************/
---
+++ I2C files/UART/obj/default/hello_ucosii.d
@@ -0,0 +1,42 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+ ../UART_bsp//HAL/inc/includes.h ../UART_bsp//HAL/inc/os_cpu.h \
+ ../UART_bsp//HAL/inc/sys/alt_irq.h ../UART_bsp//HAL/inc/nios2.h \
+ ../UART_bsp//HAL/inc/alt_types.h ../UART_bsp/system.h \
+ ../UART_bsp/linker.h ../UART_bsp//HAL/inc/priv/alt_legacy_irq.h \
+ ../UART_bsp//HAL/inc/sys/alt_irq.h ../UART_bsp//UCOSII/inc/os_cfg.h \
+ ../UART_bsp//HAL/inc/sys/alt_alarm.h \
+ ../UART_bsp//HAL/inc/sys/alt_llist.h \
+ ../UART_bsp//HAL/inc/priv/alt_alarm.h ../UART_bsp//UCOSII/inc/ucos_ii.h \
+ ../UART_bsp//HAL/inc/os_cpu.h i2c_opencores.h
+
+../UART_bsp//HAL/inc/includes.h:
+
+../UART_bsp//HAL/inc/os_cpu.h:
+
+../UART_bsp//HAL/inc/sys/alt_irq.h:
+
+../UART_bsp//HAL/inc/nios2.h:
+
+../UART_bsp//HAL/inc/alt_types.h:
+
+../UART_bsp/system.h:
+
+../UART_bsp/linker.h:
+
+../UART_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../UART_bsp//HAL/inc/sys/alt_irq.h:
+
+../UART_bsp//UCOSII/inc/os_cfg.h:
+
+../UART_bsp//HAL/inc/sys/alt_alarm.h:
+
+../UART_bsp//HAL/inc/sys/alt_llist.h:
+
+../UART_bsp//HAL/inc/priv/alt_alarm.h:
+
+../UART_bsp//UCOSII/inc/ucos_ii.h:
+
+../UART_bsp//HAL/inc/os_cpu.h:
+
+i2c_opencores.h:
