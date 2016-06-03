9d8e2f2703236cd0e787cfd8049487ef98da6968
maestroke <formaestroke@gmail.com>
2016-06-03 12:58:35 +0200

    werkende code naar master

---
+++ newtest/obj/default/hello_ucosii.d
@@ -0,0 +1,59 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+ ../newtest_bsp//HAL/inc/includes.h ../newtest_bsp//HAL/inc/os_cpu.h \
+ ../newtest_bsp//HAL/inc/sys/alt_irq.h ../newtest_bsp//HAL/inc/nios2.h \
+ ../newtest_bsp//HAL/inc/alt_types.h ../newtest_bsp/system.h \
+ ../newtest_bsp/linker.h ../newtest_bsp//HAL/inc/priv/alt_legacy_irq.h \
+ ../newtest_bsp//HAL/inc/sys/alt_irq.h \
+ ../newtest_bsp//UCOSII/inc/os_cfg.h \
+ ../newtest_bsp//HAL/inc/sys/alt_alarm.h \
+ ../newtest_bsp//HAL/inc/sys/alt_llist.h \
+ ../newtest_bsp//HAL/inc/priv/alt_alarm.h \
+ ../newtest_bsp//UCOSII/inc/ucos_ii.h ../newtest_bsp//HAL/inc/os_cpu.h \
+ ../newtest_bsp//drivers/inc/altera_up_ps2_keyboard.h \
+ ../newtest_bsp//drivers/inc/altera_up_avalon_ps2.h \
+ ../newtest_bsp//HAL/inc/sys/alt_dev.h \
+ ../newtest_bsp//HAL/inc/sys/alt_llist.h \
+ ../newtest_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../newtest_bsp//drivers/inc/altera_up_avalon_ps2.h
+
+../newtest_bsp//HAL/inc/includes.h:
+
+../newtest_bsp//HAL/inc/os_cpu.h:
+
+../newtest_bsp//HAL/inc/sys/alt_irq.h:
+
+../newtest_bsp//HAL/inc/nios2.h:
+
+../newtest_bsp//HAL/inc/alt_types.h:
+
+../newtest_bsp/system.h:
+
+../newtest_bsp/linker.h:
+
+../newtest_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../newtest_bsp//HAL/inc/sys/alt_irq.h:
+
+../newtest_bsp//UCOSII/inc/os_cfg.h:
+
+../newtest_bsp//HAL/inc/sys/alt_alarm.h:
+
+../newtest_bsp//HAL/inc/sys/alt_llist.h:
+
+../newtest_bsp//HAL/inc/priv/alt_alarm.h:
+
+../newtest_bsp//UCOSII/inc/ucos_ii.h:
+
+../newtest_bsp//HAL/inc/os_cpu.h:
+
+../newtest_bsp//drivers/inc/altera_up_ps2_keyboard.h:
+
+../newtest_bsp//drivers/inc/altera_up_avalon_ps2.h:
+
+../newtest_bsp//HAL/inc/sys/alt_dev.h:
+
+../newtest_bsp//HAL/inc/sys/alt_llist.h:
+
+../newtest_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../newtest_bsp//drivers/inc/altera_up_avalon_ps2.h:
